e6ed1e768fa04723e7102419157176487a7ba21c
Maciej-Clanet <43215153+Maciej-Clanet@users.noreply.github.com>
2018-09-12 18:18:14 +0100

    first commit

---
+++ DiscordBotCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace DiscordBotCore
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+}
